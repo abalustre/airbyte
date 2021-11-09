3ddb2f681c6ad5946de42f1b554262a7f6aa42cf
Cesar Sampaio <vinicius_spr@hotmail.com>
2021-11-09 13:47:01 -0400

    Index to false

--- airbyte-integrations/connectors/source-http-request/source_http_request/source.py
+++ airbyte-integrations/connectors/source-http-request/source_http_request/source.py
@@ -179,7 +179,7 @@ class HttpRequest(HttpStream):
                             yield value
         elif self._response_format == "xlsx":
             pd.read_excel(response.content,  dtype=str, index_col=None).to_csv('temp.csv', encoding='utf-8', index=False)
-            data = csv.DictReader(pd.read_csv('./temp.csv', dtype=str, delimiter=self._response_delimiter).to_string().splitlines(), dialect='excel')
+            data = csv.DictReader(pd.read_csv('./temp.csv', dtype=str, delimiter=self._response_delimiter).to_string(index=False).splitlines(), dialect='excel')
             yield from data
         else:
             raise Exception("Invalid response format")
